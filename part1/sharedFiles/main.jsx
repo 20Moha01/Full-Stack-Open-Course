da81b3875aa9e42366342faad6f2a1ce47104784
Mohamed El Kanboui Bouaacha <73068695+20Moha01@users.noreply.github.com>
2025-12-29 14:04:07 +0100

    Create shared main.jsx for React applications
    This file serves as the shared entry point for multiple exercises.

---
+++ part1/sharedFiles/main.jsx
@@ -0,0 +1,8 @@
+// Shared main.jsx file between courseinfo, anecdotes and unicafe exercises.
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
